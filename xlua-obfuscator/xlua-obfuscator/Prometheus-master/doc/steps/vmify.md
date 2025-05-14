dc14d0090f8a2b2620925f6714943e1fd8bc2a67
xlua-obf <xerichub@gmail.com>
2025-05-14 16:20:37 +0300

    Delete xlua-obfuscator/xlua-obfuscator directory

--- xlua-obfuscator/xlua-obfuscator/Prometheus-master/doc/steps/vmify.md
+++
@@ -1,9 +0,0 @@
----
-description: This Step will Compile your script and run it within a Vm
----
-
-# Vmify
-
-### Settings
-
-None
--- xlua-obfuscator/xlua-obfuscator/Prometheus-master/tests/loops.lua
+++
@@ -1,8 +0,0 @@
-local x = {};
-for i = 1, 100 do
-    x[i] = i;
-end
-
-for i, v in ipairs(x) do
-    print("x[" .. i .. "] = " .. v);
-end
